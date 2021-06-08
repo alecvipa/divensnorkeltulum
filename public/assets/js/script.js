a8a075fd11a92b3a008fa9c2b476f9e065685034
alecvipa <hebrit_626@hotmail.com>
2021-06-08 16:39:46 -0500

    all inclusive, except privacy policy

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,6 @@
 $(".contactUsInicioBtn, .serviceImg2, .contactUsInicioBtn2").click(function(){
     window.open("https://api.whatsapp.com/send?phone=529841468507");
 });
+$(".bannerDiving, .bannerDivingInsta").click(function(){
+    window.open("https://www.instagram.com/diveandsnorkeltulum/?hl=es");
+});
